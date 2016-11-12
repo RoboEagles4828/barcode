252a08676324df2c5ce3d34e5cde875afa4e6c05
Jackie Chen <cheezballz3.14@gmail.com>
2016-11-11 21:43:28 -0500

    Add Prints

--- barcodebrain.py
+++ barcodebrain.py
@@ -6,6 +6,8 @@ name = []
 
 
 def idconvert(roster_file, time_file):
+    print "--- STARTING ID CONVERSION ---"
+
     namefile = open(roster_file, "r")
     timefile = open(time_file, "r+b")
 
@@ -17,20 +19,20 @@ def idconvert(roster_file, time_file):
     for lines in namereader:
         name.append(lines)
 
-    print time
-    print name
-
     timefile.seek(0)
 
     for line in time:
         for key in name:
             if line[0] == key[0]:
+                print "Change " + line[0] + " to " + key[1]
                 timefile.write(str(key[1]) + "," + str(line[1]) + "\n")
     namefile.close()
     timefile.close()
+    print "--- FINISHED ID CONVERSION ---"
 
 
 def getid():
+    print "--- STARTING ID FETCH ---"
     filename = "attendance/attendance" + str(
         datetime.date.strftime(datetime.datetime.today().date(), "%m-%d-%y")) + ".csv"
     print "Log Name: " + filename
@@ -46,10 +48,12 @@ def getid():
         print "Thank you, User " + num
         idwriter.writerow([num, datetime.time.strftime(datetime.datetime.now().time(), "%I:%M:%S")])
     idfile.close()
+    print "--- FINISHED ID FETCH ---"
     return filename
 
 
 def findtimediffs(time_file):
+    print "--- STARTING TIME DIFF ---"
     times = []
     queue = [[""]]
     gotname = False
@@ -59,20 +63,19 @@ def findtimediffs(time_file):
         times.append(lines)
     timefile.seek(0, 2)
     timefile.write("\n")
-    print times
     for line in times:
-        print queue
-        print line
         for person in queue:
-            print person[0]
             if line[0] == person[0]:
-                diff = datetime.datetime.strptime(
-                    line[1], '%I:%M:%S') - datetime.datetime.strptime(
-                    person[1], '%I:%M:%S')
-                print diff
+                time1 = datetime.datetime.strptime(line[1], '%I:%M:%S')
+                time2 = datetime.datetime.strptime(person[1], '%I:%M:%S')
+                print line[0] + ": Got times " + str(time1.strftime("%I:%M:%S")) + " and " + str(time2.strftime("%I:%M:%S"))
+                diff =  time1 - time2
+                print line[0] + ": Got diff " + str(diff)
                 timefile.write(str(line[0]) + "," + str(diff) + "\n")
                 queue.remove(person)
                 gotname = True
         if not gotname:
+            print line[0] + ": Added to queue"
             queue.append(line)
     timefile.close()
+    print "--- FINISHED TIME DIFF ---"
