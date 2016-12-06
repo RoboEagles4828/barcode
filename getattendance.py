e9ad28bdd60981fa8f89a30a8f13f75d89a2d110
Pavan <dayalpavan@gmail.com>
2016-12-06 15:47:48 -0500

    creates JSON of all attendance data

---
+++ getattendance.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import os
+import record
+
+attendance_folder = "attendance"
+roster_file = "roster.csv"
+
+#returns True if the csv file doesn't end in "-sum" and is a ".csv"
+def valid_attendance_file(f):
+	of = record.output_format
+	return not record.sum_extension in f and f[-len(of):] == of
+
+#create attendance directory if not already made
+if not os.path.exists(attendance_folder):
+	os.makedirs(attendance_folder)
+
+#get list of attendance ".csv" files that don't end in "-sum.csv"
+files = [f.replace(record.output_format, "") for f in os.listdir(attendance_folder) if valid_attendance_file(f)]
+
+#append json for each file to final json
+json = '{'
+for file in files:
+	json += record.Record(roster_file, attendance_folder, True, file).toJSON()
+	json += ","
+json = json[:-1] + "}"
+
+#print out final json
+print(json)
